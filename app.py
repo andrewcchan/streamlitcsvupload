98123810fe3bcd9b2ef5b639e3dceb4de9b70cd0
Andrew Chan <andrewchan@andrews-mbp-14.lan>
2024-09-12 08:01:04 -0700

    add plot

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-from io import BytesIO
-import matplotlib.pyplot as plt
-
-uploaded_files = st.file_uploader(
-    "Choose a CSV file", accept_multiple_files=True
-)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
-    df = pd.read_csv(BytesIO(bytes_data)) 
-    st.write(df)
-#    # Create a figure and axes
-#     fig, ax = plt.subplots()
-
-#     # Plot the data
-#     ax.plot(df['Date'], df['Value'])
-
-#     # Set labels and title
-#     ax.set_xlabel('X-axis')
-#     ax.set_ylabel('Y-axis')
-#     ax.set_title('Simple Plot')
-
-    # Display the plot in Streamlit
-    # st.pyplot(df['Date'], df['Value'])
-    st.scatter_chart(df[["Date","Value"]],x='Date',y='Value')
+import plotly.express as px
+# create random x and y values
+st.title("Fitbit Step Forecasting")
+start_date = pd.to_datetime('2024-01-01')
+dates = pd.date_range(start=start_date, periods=1000, freq='D')
+y = [30 * np.sin(2 * np.pi * i / 100) + 10 * np.random.randn() for i, _ in enumerate(dates)]
+y_hat = [30 * np.sin(2 * np.pi * i / 100) for i, _ in enumerate(dates)]
+df = pd.DataFrame({'Date':dates, 'step count':y, 'predicted step count':y_hat})
+fig = px.scatter(df, x='Date', y=['step count', 'predicted step count'], color_discrete_map={'predicted step count':'orange'})
+fig.update_yaxes(title_text='Step count')
+st.plotly_chart(fig, use_container_width=True)
 
 
     # https://www.statsmodels.org/stable/generated/statsmodels.tsa.arima.model.ARIMA.html
